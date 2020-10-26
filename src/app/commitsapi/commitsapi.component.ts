9f0bf1d9bac8de1e2702371a6cec1b6389274437
jhansi <jhansiravula33@gmail.com>
2020-10-26 17:56:41 +0530

    printing the data in console,using typescript for loop

--- src/app/commitsapi/commitsapi.component.ts
+++ src/app/commitsapi/commitsapi.component.ts
@@ -8,7 +8,7 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./commitsapi.component.css']
 })
 export class CommitsapiComponent implements OnInit {
-  commitdata=[];
+  commitdata: Object;
 
   constructor(private http: HttpClient) { }
 
@@ -17,6 +17,18 @@ export class CommitsapiComponent implements OnInit {
     this.http.get('https://api.github.com/repositories/19438/commits',)
       .subscribe((res: any) => {
         this.commitdata = res;
+        //console.log(res);    //display all the results
+
+        //for (let items in res)
+        for (var i = 0; i < res.length; i++) {
+          console.log(res[i].author.id);
+          console.log(res[i].author.avatar_url);       //printing only urls
+          console.log(res[i].commit.author.name);
+          console.log(res[i].commit.author.email);
+          console.log(res[i].commit.author.date);
+          //console.log(res[i]);     //display all the results
+        }
+
       })
   }
 }
